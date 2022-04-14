892ca7febe742e868b2c62c709d387be7a07b282
Sally Sghair <sallysghair1@gmail.com>
2022-04-14 16:09:08 +0100

    setup proxy front/back communicating

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React from 'react'
+import axios from 'axios'
+
+
+
 function App() {
-  return <h1>Hello World</h1>
+  React.useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get('/api/pokemon')
+      console.log(res.data)
+    }
+    getData()
+  })
+  return <h1>hello</h1>
 }
 
 export default App
